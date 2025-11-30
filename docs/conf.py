df70103023ccc57ab3bb7e580de57cfa6aa54484
A0_12424828 <cynthia.ani@student.tugraz.at>
2025-11-30 19:34:12 +0100

    Configure Sphinx with autodoc and src path

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+# Add the src directory so Sphinx can import mypkg
+sys.path.insert(0, os.path.abspath("../src"))
+
+project = 'swdev-ruff-pipeline'
+copyright = '2025, Cynthia Ani'
+author = 'Cynthia Ani'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
